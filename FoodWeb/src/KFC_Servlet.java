4ff5b7a99d0c263a6a97a15825623f8a0a0c159a
2014068876 <2014068876@ust-ics.mygbiz.com>
2016-05-04 23:09:17 +0800

    Added files via upload

---
+++ FoodWeb/src/KFC_Servlet.java
@@ -0,0 +1,83 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class KFC_Servlet
+ */
+@WebServlet("/KFC_Servlet")
+public class KFC_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<title>FoodbUST</title>");
+		
+		writer.println("<style>");
+		writer.println("ul {");
+		writer.println("list-style-type: none;");
+		writer.println("margin: 0;");
+		writer.println("padding: 0;");
+		writer.println("overflow: hidden;");
+		writer.println("background-color: #333;");
+		writer.println("}");
+		writer.println("li {");
+		writer.println("float: left;");
+		writer.println("}");
+
+		writer.println(" li a {");
+		writer.println(" display: block;");
+		writer.println(" color: white;");
+		writer.println(" text-align: center;");
+		writer.println("    padding: 14px 16px;");
+		writer.println("text-decoration: none;");
+		writer.println("  }");
+
+		writer.println(" li a:hover {");
+		writer.println("     background-color: #111;");
+		writer.println("    }");
+		writer.println("</style>");
+		
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<img src = 'images/HEADER.jpg' width = '100%'>");
+		
+		writer.println("<ul>");
+		writer.println("  <li><a class='active' href='index.html'>Home</a></li>");
+		writer.println("  <li><a href='HTMLRestoList.html'>Restaurants</a></li>");
+		writer.println("  <li><a href='#contact'>Contact Us</a></li>");
+		writer.println("  <li><a href='#about'>About</a></li>");
+		writer.println("  </ul>");
+		
+		writer.println("<img src = 'images/kfc.jpg'>");
+		writer.println("<p>Kentucky Fried Chicken is a fast food restaurant chain that" +
+			"specializes in fried chicken and is headquartered in Loiusville, Kentucky," +
+			"in the United States. It is the world's second largest restaurant chain after" +
+			"McDonald's with 18 875 outlets in 118 countries and territories as of December 2013.</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ FoodWeb/src/Subway_Servlet.java
@@ -0,0 +1,79 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Subway_Servlet
+ */
+@WebServlet("/Subway_Servlet")
+public class Subway_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<title>FoodbUST</title>");
+		
+		writer.println("<style>");
+		writer.println("ul {");
+		writer.println("list-style-type: none;");
+		writer.println("margin: 0;");
+		writer.println("padding: 0;");
+		writer.println("overflow: hidden;");
+		writer.println("background-color: #333;");
+		writer.println("}");
+		writer.println("li {");
+		writer.println("float: left;");
+		writer.println("}");
+
+		writer.println(" li a {");
+		writer.println(" display: block;");
+		writer.println(" color: white;");
+		writer.println(" text-align: center;");
+		writer.println("    padding: 14px 16px;");
+		writer.println("text-decoration: none;");
+		writer.println("  }");
+
+		writer.println(" li a:hover {");
+		writer.println("     background-color: #111;");
+		writer.println("    }");
+		writer.println("</style>");
+		
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<img src = 'images/HEADER.jpg' width = '100%'>");
+		
+		writer.println("<ul>");
+		writer.println("  <li><a class='active' href='index.html'>Home</a></li>");
+		writer.println("  <li><a href='HTMLRestoList.html'>Restaurants</a></li>");
+		writer.println("  <li><a href='#contact'>Contact Us</a></li>");
+		writer.println("  <li><a href='#about'>About</a></li>");
+		writer.println("  </ul>");
+		
+		writer.println("<img src = 'images/subway.jpg'>");
+		writer.println("<p>Subway is an American fast food restaurant franchise that primarily sells submarine sandwiches and salads.</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
